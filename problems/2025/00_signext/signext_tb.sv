b2b06fbb12fb9de71bd12f773078a3d6fc2fa563
Anastasia <kulevich05@gmail.com>
2025-02-12 15:49:51 +0300

    Modify testbench.

--- problems/2025/00_signext/signext_tb.sv
+++ problems/2025/00_signext/signext_tb.sv
@@ -2,8 +2,8 @@
 
 module signext_tb;
 
-localparam int N = 3;
-localparam int M = 5;
+localparam int N = 4;
+localparam int M = 6;
 
 logic [N-1:0] init_val = 0;
 logic [M-1:0]  ext_val;
@@ -20,16 +20,17 @@ always begin
     init_val = init_val + 1;
     #1;
     if ({{M-N{init_val[N-1]}}, init_val} == ext_val) begin
-        $display("OKAY val = %b, ext = %b", {{M-N{init_val[N-1]}}, init_val}, ext_val);
+        //$display("OKAY val = %b, ext = %b", {{M-N{init_val[N-1]}}, init_val}, ext_val);
     end else begin
         $display("NOT OKAY val = %b, ext = %b", {{M-N{init_val[N-1]}}, init_val}, ext_val);
     end
+
+    if (init_val == (2**N - 1))
+        $finish();
 end
 
 initial begin
     $dumpvars;
-    #10; // Testing time
-    $finish;
 end
 
 endmodule
