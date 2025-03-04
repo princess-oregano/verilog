ebb2bdffe947f91f1fc636708b9255ec5f7e0f95
Anastasia <kulevich05@gmail.com>
2025-03-04 23:41:16 +0300

    Add timer.

---
+++ problems/2025/03_timer/timer.v
@@ -0,0 +1,26 @@
+module timer #(
+    parameter COUNT = 600
+)(
+    input wire clk,
+    input wire rst_n,
+    input wire en,
+
+    output reg [WIDTH-1:0] o_cnt
+);
+
+localparam WIDTH = $clog2(COUNT);
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        o_cnt <= COUNT;
+    end
+    else if (en) begin
+        if (o_cnt)
+            o_cnt <= o_cnt - 1;
+        else
+            o_cnt <= COUNT;
+    end
+end
+
+endmodule
+
